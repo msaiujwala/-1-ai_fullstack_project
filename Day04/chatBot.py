698c99974f86aae2525d5832fa2ff9b28154080f
msaiujwala <msaiujwala@gmail.com>
2026-09-24 12:14:05 +0530

    Day 4: Chatbot with memory

---
+++ Day04/chatBot.py
@@ -0,0 +1,26 @@
+import ollama
+msgs = [
+    {"role": "system",
+    "content": "Give the answers in simple terms."}
+]
+while True:
+    question = input("You: ")
+    if question.lower() == "exit":
+        break
+    msgs.append(
+        {"role": "user",
+        "content": question}
+    )
+
+    response = ollama.chat(
+        model="llama3.2:3b",
+        messages=msgs)
+    msgs.append(
+        {"role": "assistant",
+        "content": response["message"]["content"]}
+    )
+    print("AI:", response["message"]["content"])
+
+print("----Chat History----\n")
+for msg in msgs:
+    print(msg["role"],":",msg["content"])
